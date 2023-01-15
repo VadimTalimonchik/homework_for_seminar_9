36e1acb8c68a735d32a7aea58c2d0e1cfb610462
VadimTalimonchik <tvadmins@gmail.com>
2023-01-15 13:30:54 +0300

    Задача 66

--- hw_66/Program.cs
+++ hw_66/Program.cs
@@ -4,4 +4,19 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.WriteLine("Hello, World!");
+int m = InputInt("Введите значение M: ");
+int n = InputInt("Введите значение N: ");
+Console.WriteLine($"Сумма натуральных элементов от {m} до {n} равна {NaturalNumbersSum(m, n)}.");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int NaturalNumbersSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NaturalNumbersSum(m, n - 1);
+}
