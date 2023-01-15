ae72d8a5ab9f6886b1c9e7b28ba8f991fad6c5ee
VadimTalimonchik <tvadmins@gmail.com>
2023-01-15 13:50:01 +0300

    Задача 68

--- hw_68/Program.cs
+++ hw_68/Program.cs
@@ -4,9 +4,8 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-Console.WriteLine("Введите два положительных числа: M и N.");
-int m = InputInt("Введите M: ");
-int n = InputInt("Введите N: ");
+int m = InputInt("Введите m: ");
+int n = InputInt("Введите n: ");
 Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
 
 int InputInt(string output)
